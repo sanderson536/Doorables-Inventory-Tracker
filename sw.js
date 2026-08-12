cce6850de7bebfe0b0230543444ebbd0b60b2080
sanderson536 <sanderson536@yahoo.com>
2026-08-12 10:27:14 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'steves-doorable-shell-v1-rc1-gh-pages'
+const CACHE_NAME = 'steves-doorable-shell-rc1-hotfix1'
 const SCOPE_URL = self.registration.scope
 const SCOPE_PATH = new URL(SCOPE_URL).pathname
 const APP_SHELL = [
